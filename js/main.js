356bdb394d91d66acff5e7c7df5f11ba77c7f108
Emanuela Dance <emanuelladance@gmail.com>
2020-02-10 15:56:31 +0200

    Added automatic frequency mapping to notes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // const fs = require('fs');
     // function readSong() {
     //     let notes = [];
@@ -17,12 +17,12 @@ $(document).ready(function() {
     // }
 
 
-    function getSong(){
+    function getSong() {
         // let notes = "b c c c c c c c a a b c c c b c c b a a f";
         let notes = "c c d c f e c c d c g f c c c a f e d b b a f g f";
-        notes=notes.toUpperCase();
-        let newNotes=[];
-        let prev = {"A":"G", "B":"A","C":"B","D":"C","E":"D","F":"E","G":"F",};
+        notes = notes.toUpperCase();
+        let newNotes = [];
+        let prev = {"A": "G", "B": "A", "C": "B", "D": "C", "E": "D", "F": "E", "G": "F",};
         // let flats = "a b d e";
         let flats = "b";
         flats = flats.toUpperCase();
@@ -30,9 +30,9 @@ $(document).ready(function() {
         notes = notes.split(" ");
         console.log(flats);
         console.log(notes);
-        for (let note of notes){
+        for (let note of notes) {
             console.log(note);
-            if (flats.includes(note)){
+            if (flats.includes(note)) {
                 note = prev[note] + "#";
             }
             newNotes.push(note);
@@ -58,42 +58,87 @@ $(document).ready(function() {
         navigator.mediaDevices = {};
     }
 
-    $("#start").on("click",function () {startRecording();});
-    $("#stop").on("click",function () {stopRecording();});
+    $("#start").on("click", function () {
+        startRecording();
+    });
+    $("#stop").on("click", function () {
+        stopRecording();
+    });
 
     const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     audioCtx.sampleRate = 44100;
     const biquadFilter = audioCtx.createBiquadFilter();
     biquadFilter.type = "lowshelf";
     const analyser = audioCtx.createAnalyser();
-    analyser.minDecibels = -100;
-    analyser.maxDecibels = -30;
-    analyser.smoothingTimeConstant = 0.92;
+    // only useful for getByteFrequencyData
+    // analyser.minDecibels = -70;
+    // analyser.maxDecibels = -10;
+    analyser.smoothingTimeConstant = 0.89;
     let bufferLength = analyser.frequencyBinCount;
     console.log(bufferLength);
     let dataArray = new Uint8Array(bufferLength);
+    let frequencies = {};
+    let notes = {};
+    analyser.fftSize = 4096;
+    const bandSize = audioCtx.sampleRate / analyser.fftSize;
+
+
+    function mapFrequencies(notes, frequencies) {
+        const a = Math.pow(2, 1 / 12);
+        const note_names = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+        frequencies[57] = 440;
+        notes[440] = "A4";
+        let k = 58;
+        let note = 1;
+        for (let i = 4; i < 9;) {
+            frequencies[k] = frequencies[k - 1] * a;
+            notes[frequencies[k]] = note_names[note] + i.toString();
+            k++;
+            note++;
+            if (note === 3)
+                i++;
+            if (note === 12)
+                note = 0;
+        }
+        k = 56;
+        note = 11;
+        for (let i = 4; i >= 0;) {
+            frequencies[k] = frequencies[k + 1] / a;
+            notes[frequencies[k]] = note_names[note] + i.toString();
+            k--;
+            note--;
+            if (note === 2)
+                i--;
+            if (note < 0)
+                note = 11;
+        }
+
+        console.log(notes);
+        console.log(frequencies);
+    }
 
     // Audio Buffer - to access the raw PCM data (in case we need it)
     // let myArrayBuffer = audioCtx.createBuffer(2, audioCtx.sampleRate, audioCtx.sampleRate);
 
     function stopRecording() {
-        audioCtx.suspend().then(()=>{
+        audioCtx.suspend().then(() => {
             running = false;
         })
 
     }
 
-    function startRecording(){
+    function startRecording() {
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             console.log('getUserMedia supported.');
-            navigator.mediaDevices.getUserMedia (
+            navigator.mediaDevices.getUserMedia(
                 // constraints - only audio needed for this app
                 {audio: true})
             // Success callback
-                .then(function(stream) {
+                .then(function (stream) {
                     console.log("Success");
                     running = true;
-                    audioCtx.resume().then(()=> {
+                    mapFrequencies(notes, frequencies);
+                    audioCtx.resume().then(() => {
                         const microphone = audioCtx.createMediaStreamSource(stream);
                         // microphone.connect(analyser);
                         microphone.connect(biquadFilter);
@@ -103,29 +148,154 @@ $(document).ready(function() {
                     });
                 })
                 // Error callback
-                .catch(function(err) {console.log('The following getUserMedia error occurred: ' + err);});
-        } else {console.log('getUserMedia not supported on your browser!');}
+                .catch(function (err) {
+                    console.log('The following getUserMedia error occurred: ' + err);
+                });
+        } else {
+            console.log('getUserMedia not supported on your browser!');
+        }
     }
 
-    const notes = {
-        11:"C", 22:"C",43:"C", 44:"C", 45:"C", 78:"C", 112:"C", 202:"C", 248:"C",
-        23:"C#", 46:"C#", 47:"C#", 48:"C#", 83:"C#",95:"C#", 119:"C#",
-        12:"D", 87:"D",88:"D", 24:"D",25:"D", 49:"D", 50:"D", 51:"D",227:"D",240:"D",266:"D",
-        26:"D#", 27:"D#", 52:"D#", 53:"D#", 93:"D#",106:"D#",
-        13:"E", 28:"E", 54:"E", 55:"E", 56:"E",57:"E",99:"E", 199:"E", 84:"E",
-        14:"F",29:"F", 30:"F", 58:"F",59:"F", 60:"F", 61:"F",149:"F",
-        15:"F#",31:"F#", 32:"F#", 62:"F#",63:"F#", 64 :"F#", 141:"F#",
-        16:"G", 33:"G",34:"G",65:"G",66:"G",  67:"G", 92:"G",117:"G", 185:"G", 203:"G", 255:"G",
-        17:"G#", 35:"G#", 36:"G#", 69:"G#",68:"G#", 75:"G#",76:"G#", 101:"G#",142:"G#",
-        9:"A", 18:"A", 19:"A", 37:"A", 38:"A", 70:"A", 71:"A", 85:"A", 94:"A",150:"A",317:"A",318:"A",
-        10:"A#", 20:"A#",39:"A#", 40:"A#", 72:"A#",73:"A#",79:"A#",80:"A#",89:"A#",90:"A#", 131:"A#",132:"A#",
-        21:"B", 41:"B", 42:"B",86:"B",115:"B", 116:"B", 125:"B",126:"B",136:"B",137:"B",252:"B",253:"B",
+    const notes1 = {
+        11: "C",
+        22: "C",
+        43: "C",
+        44: "C",
+        45: "C",
+        78: "C",
+        112: "C",
+        202: "C",
+        248: "C",
+        23: "C#",
+        46: "C#",
+        47: "C#",
+        48: "C#",
+        83: "C#",
+        95: "C#",
+        119: "C#",
+        12: "D",
+        87: "D",
+        88: "D",
+        24: "D",
+        25: "D",
+        49: "D",
+        50: "D",
+        51: "D",
+        227: "D",
+        240: "D",
+        266: "D",
+        26: "D#",
+        27: "D#",
+        52: "D#",
+        53: "D#",
+        93: "D#",
+        106: "D#",
+        13: "E",
+        28: "E",
+        54: "E",
+        55: "E",
+        56: "E",
+        57: "E",
+        99: "E",
+        199: "E",
+        84: "E",
+        14: "F",
+        29: "F",
+        30: "F",
+        58: "F",
+        59: "F",
+        60: "F",
+        61: "F",
+        149: "F",
+        15: "F#",
+        31: "F#",
+        32: "F#",
+        62: "F#",
+        63: "F#",
+        64: "F#",
+        141: "F#",
+        16: "G",
+        33: "G",
+        34: "G",
+        65: "G",
+        66: "G",
+        67: "G",
+        92: "G",
+        117: "G",
+        133: "G",
+        134: "G",
+        185: "G",
+        203: "G",
+        255: "G",
+        17: "G#",
+        35: "G#",
+        36: "G#",
+        69: "G#",
+        68: "G#",
+        75: "G#",
+        76: "G#",
+        101: "G#",
+        142: "G#",
+        9: "A",
+        18: "A",
+        19: "A",
+        37: "A",
+        38: "A",
+        70: "A",
+        71: "A",
+        85: "A",
+        94: "A",
+        150: "A",
+        189: "A",
+        317: "A",
+        318: "A",
+        10: "A#",
+        20: "A#",
+        39: "A#",
+        40: "A#",
+        72: "A#",
+        73: "A#",
+        79: "A#",
+        80: "A#",
+        89: "A#",
+        90: "A#",
+        131: "A#",
+        132: "A#",
+        21: "B",
+        41: "B",
+        42: "B",
+        86: "B",
+        115: "B",
+        116: "B",
+        125: "B",
+        126: "B",
+        136: "B",
+        137: "B",
+        252: "B",
+        253: "B",
     };
 
-    function v(){
+    function binarySearch(frequencies, min, max) {
+        var size = Object.keys(frequencies).length;
+        let start = 0, end = size - 1;
+        while (start <= end) {
+            let mid = Math.floor((start + end) / 2);
+            let elem = frequencies[mid];
+            if (elem >= min && elem <= max)
+                return elem;
+            else if (elem < min){
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return "idk";
+    }
+
+    function v() {
         const HEIGHT = 100;
         const WIDTH = 300;
-        analyser.fftSize = 4096;
         bufferLength = analyser.frequencyBinCount;
         dataArray = new Uint8Array(bufferLength);
         const fDataArray = new Float32Array(bufferLength);
@@ -135,7 +305,7 @@ $(document).ready(function() {
         canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
         let drawVisual;
-        const draw1 = function() {
+        const draw1 = function () {
             if (running)
                 drawVisual = requestAnimationFrame(draw1);
             analyser.getByteFrequencyData(dataArray);
@@ -143,15 +313,26 @@ $(document).ready(function() {
             // console.log(getMax(fDataArray));
             // console.log(getMax(dataArray));
             let f = getIndexOfMax(fDataArray);
-            console.log("freq " + f);
-            console.log(notes[f]);
-            $("#note").text(notes[f]);
+            // console.log("freq " + f);
+            // f = index
+            let min = f * bandSize;
+            let max = min + bandSize;
+            let frequency = binarySearch(frequencies, min, max);
+            console.log("NEW FRAME");
+            console.log(f);
+            console.log(bandSize);
+            console.log(min);
+            console.log(max);
+            console.log(frequency);
+            console.log(notes[frequency]);
+            // console.log(notes1[f]);
+            $("#note").text(notes1[f]);
 
-            if(notes[f]===song[crt]){
-                crt_song.append(" " + notes[f]);
+            if (notes1[f] === song[crt]) {
+                crt_song.append(" " + notes1[f]);
                 crt++;
-                if (crt >= song.length){
-                    $("#congrats").css("visibility","visible");
+                if (crt >= song.length) {
+                    $("#congrats").css("visibility", "visible");
                     stopRecording();
                 }
             }
@@ -181,21 +362,20 @@ $(document).ready(function() {
     }
 
 // Get the highest 2 spikes and return the lowest one
-    function getIndexOfMax(data){
-        let max, smax, k1=0, k2=0;
+    function getIndexOfMax(data) {
+        let max, smax, k1 = 0, k2 = 0;
         max = data[0] > data[1] ? data[0] : data[1];
         smax = data[0] > data[1] ? data[1] : data[0];
-        for (let i = 2; i < data.length; i++){
+        for (let i = 2; i < data.length; i++) {
             let val = data[i];
-            if(val>max){
+            if (val > max) {
                 const tmp = max;
                 max = val;
                 smax = tmp;
                 k2 = k1;
                 k1 = i;
 
-            }
-            else if (val > smax){
+            } else if (val > smax) {
                 smax = val;
                 k2 = i;
             }
@@ -204,15 +384,15 @@ $(document).ready(function() {
         return k1 > k2 ? k2 : k1;
     }
 
-    function enumerateDevices(){
+    function enumerateDevices() {
         navigator.mediaDevices.enumerateDevices()
-            .then(function(devices) {
-                devices.forEach(function(device) {
+            .then(function (devices) {
+                devices.forEach(function (device) {
                     console.log(device.kind + ": " + device.label +
                         " id = " + device.deviceId);
                 });
             })
-            .catch(function(err) {
+            .catch(function (err) {
                 console.log(err.name + ": " + err.message);
             });
     }
